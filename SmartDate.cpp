78cbdff42feb2750913fb1d23c895388dd0159c1
Maxim <max@huzm.ru>
2023-04-25 21:11:27 +0300

    The class moved to header file

--- SmartDate.cpp
+++ SmartDate.cpp
@@ -3,24 +3,7 @@
 */
 
 #include <iostream>
-
-class SmartDate
-{
-private:
-    short day;
-    short month;
-    short year;
-public:
-    SmartDate(short _d, short _m, short _y);
-};
-
-SmartDate::SmartDate(short _d, short _m, short _y)
-{
-    day = _d;
-    month = _m;
-    year = _y;
-}
-
+#include "SmartDate.h"
 
 int main()
 {
